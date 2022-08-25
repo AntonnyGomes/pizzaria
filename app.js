e74ee73facd15dae124604f418beeb0ee67d0b87
AntonnyGomes <104103113+AntonnyGomes@users.noreply.github.com>
2022-08-25 10:35:36 -0300

    commit criando controller

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const app = express();
 
 // Importando o roteador que lida com as rotas de pizza
 const PizzasRouter = require('./routes/PizzasRouter')
+// verificando se a requisição e para a pasta public
+app.use(express.static("public"));
+
 
 // Fazendo com que a aplicação utilize o roteador para todas as req que chegarem para endereços que comecem com /pizzas
 app.use('/pizzas', PizzasRouter);
 
+app.set('view engine', 'ejs')
 // Adicionando uma rota na aplicação que responde para usuário diretamente... (isso não é MVC, mas funciona)
-app.get('/', (req,res) => {res.send("Olá, visitante")})
+app.get('/', (req, res) => { res.send("Olá, visitante") })
 
 // Pondo a aplicação para rodar escutando na porta 3000
-app.listen(3000, ()=>{console.log("servidor rodando na porta 3000")});
+app.listen(3000, () => { console.log("servidor rodando na porta 3000") });
 
